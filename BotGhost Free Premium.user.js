cf9f90c2cd2896f46b37a9840c67d23a927c3718
tuncvr <78257164+tuncvr@users.noreply.github.com>
2021-02-26 08:56:03 +0300

    Add files via upload

---
+++ BotGhost Free Premium.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         BotGhost Free Premium
+// @version      1.0
+// @description  Free premium on BotGhost!
+// @author       Moodyzoo
+// @match        https://botghost.com/*
+// @grant        none
+// @namespace https://greasyfork.org/users/690187
+// ==/UserScript==
+
+cleanbot.premium = true
+cleanbot.trial = true
+cleanbot.subscription = {id:0,sub_type:'lifetime',end:null,trial_end:false}
